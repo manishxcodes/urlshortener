5ee439113fefced2d02291e9eff388eec4e1c633
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-12-04 22:54:26 +0530

    add zod schema

---
+++ src/schema.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+export const signupSchema = z.object({
+  firstname: z
+    .string()
+    .min(1, "First name is required")
+    .max(55, "First name must be at most 55 characters"),
+
+  lastname: z
+    .string()
+    .max(55, "Last name must be at most 55 characters")
+    .optional(), // since it's not `.notNull()` in your model
+
+  email: z
+    .email("Invalid email format")
+    .max(255, "Email must be at most 255 characters"),
+
+  password: z
+    .string()
+    .min(8, "Password must be at least 8 characters")
+    .max(16, "Password must be at most 16 characters"),
+});
+
+export type SignupInputType = z.infer<typeof signupSchema>
