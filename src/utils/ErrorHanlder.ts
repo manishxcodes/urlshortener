27ee400e43a8e4db67389720f41889fd39f6962e
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-12-04 18:58:13 +0530

    add errroHandler to handle error

---
+++ src/utils/ErrorHanlder.ts
@@ -0,0 +1,24 @@
+import type { Request, Response, NextFunction } from 'express';
+import { AppError } from './AppError';
+
+export const errorHandler = (
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+) => {
+    if(err instanceof AppError) {
+        return res.status(err.statusCode).json({
+            success: false,
+            message: err.message,
+            errors: err.details || null
+        });
+    }
+
+    // default error 
+    return res.status(500).json({
+        success: false,
+        message: err.message,
+        errors: err.message || "Internal Server Error"
+    });
+}
