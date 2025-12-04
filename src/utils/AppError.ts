e1589745106dc9b9db76f5c76f9c2435fb6fdd56
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-12-04 18:40:11 +0530

    add AppError for structured error messages/response

---
+++ src/utils/AppError.ts
@@ -0,0 +1,12 @@
+export class AppError extends Error {
+    statusCode: number;
+    details?: any
+
+    constructor (message: string, statusCode = 500, details?: any) {
+        super(message);
+        this.statusCode = statusCode;
+        this.details = details;
+
+        Object.setPrototypeOf(this, AppError.prototype);
+    }
+}
