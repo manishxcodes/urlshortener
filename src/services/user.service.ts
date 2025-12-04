85a7eeac213725ea2e9a489dd5360dff42623708
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-12-05 00:54:21 +0530

    feat: add UserService class to handle bussiness logic of user

---
+++ src/services/user.service.ts
@@ -0,0 +1,49 @@
+import bcrypt from 'bcrypt';
+import { AppError } from 'utils/AppError';
+import { UserRepository } from 'repositories/user.repository';
+import type { SignupInputType } from 'schema';
+import { hash, sign } from 'crypto';
+import { signToken } from 'utils/jwt';
+
+export class UserService {
+    private repository = new UserRepository();
+
+    async signup (data: SignupInputType) {
+        const existingUser = await this.repository.findByEmail(data.email);
+        if(existingUser) throw new AppError("Email is already in use", 400);
+
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(data.password, salt);
+
+        const newUser = await this.repository.createUser({
+            firstname: data.firstname,
+            lastname: data.lastname,
+            password: hashedPassword,
+            email: data.email
+        });
+        if(!newUser) throw new AppError("Couldn't signup", 500);
+
+        const token = signToken({id: newUser.id});
+
+        const user = {
+            email: newUser.email,
+            id: newUser.id,
+            firstname: newUser.firstname,
+            lastname: newUser.lastname,
+        }
+
+        return { user, token };
+    }
+
+    async signin ({email, password}: {email: string, password: string}) {
+        const user = await this.repository.findByEmail(email);
+        if(!user) throw new AppError("User not found", 404);
+
+        const match = await bcrypt.compare(password, user.password);
+        if(!match) throw new AppError("Invalid email or password", 400);
+
+        const token = signToken({ id: user.id });
+
+        return { user, token };
+    }
+}
