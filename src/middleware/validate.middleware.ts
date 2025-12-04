71567014a809eb7008f79feed362742358035411
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-12-04 22:54:46 +0530

    feat: add middleware to validate zod type

---
+++ src/middleware/validate.middleware.ts
@@ -0,0 +1,25 @@
+import type { Response, Request, NextFunction } from "express";
+import { AppError } from "utils/AppError.ts";
+import { ZodType } from "zod";
+
+export const validate = (
+    schema: ZodType
+) => (
+    req: Request, res: Response, next: NextFunction
+) => {
+    const result = schema.safeParse(req.body);
+    if(!result.success) {
+        const errorMessage =  result.error.issues.map((err) => ({
+            path: err.path.join("."),
+            message: err.message,
+        }));
+
+        console.log("error: ", errorMessage);
+        return next(
+            new AppError(`Validation failed:`, 400, errorMessage)
+        ); 
+    }
+
+    req.body = result.data;
+    next();
+}
