d18227f0546556bb56f668fdca43acb76f9e4a93
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-12-05 04:01:05 +0530

    add url controller to handle http request

---
+++ src/controllers/url.controller.ts
@@ -0,0 +1,63 @@
+import type { Request, Response, NextFunction } from "express";
+import { UrlRepository } from "repositories/url.repository";
+import { UrlService } from "services/url.service";
+import { AppError } from "utils/AppError";
+import { AppResponse } from "utils/AppResponse";
+import { asyncHandler } from "utils/AsyncHandler";
+
+const urlService = new UrlService();
+const urlRepo = new UrlRepository();
+
+export const createUrl = asyncHandler(
+    async function (req: Request, res:Response, next: NextFunction) {
+        const userId = req.userId;
+        const targetURL = req.body.targetURL;
+        const shortCode = ""
+
+        if(!userId) return next(new AppError("Unauthorized", 401));
+
+        const data = await urlService.createUrl({
+            userId,
+            targetURL,
+            shortCode
+        })
+
+        if(!data) return next(new AppError("Couldn't generate shortUrl"));
+
+        return AppResponse.created(res, "Short url generated.", data);
+    }
+)
+
+export const getAllUrl = asyncHandler(
+    async (req: Request, res: Response, next: NextFunction) => {
+        const userId = req.userId;
+        if(!userId) return next(new AppError("Unauthorized", 401));
+
+        const urls = urlService.getAllUrl(userId);
+         if(!urls) return next(new AppError("Couldn't get all shortUrl"));
+
+        return AppResponse.success(res, "FetchedUrl", urls);
+    }
+)
+
+export const deleteUrl = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const userId = req.userId;
+    const { shortCode } = req.body;
+    if(!userId) return next(new AppError("Unauthorized", 401));
+    if(!shortCode) return next(new AppError("Shortcode required", 400));
+
+    const deletedUrl = await urlService.deleteUrl(shortCode, userId);
+    if(!deletedUrl) return next(new AppError("Couldn't delete shortUrl"));
+
+    return AppResponse.success(res, "URL deleted", deletedUrl)
+});
+
+export const redirect = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
+    const shortCode = req.params.shortCode;
+    if(!shortCode) return next(new AppError("Shortcode is required", 401));
+
+    const data = await urlRepo.findByShortCode(shortCode);
+    if(!data) return next(new AppError("Invalid url", 404));
+
+    return res.redirect(data.targetURL);
+});
