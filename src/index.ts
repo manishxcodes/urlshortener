7b101ba13db3dfeb1db9fb55a312535668fcf33e
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-12-05 01:25:31 +0530

    add cookie parser

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { errorHandler } from 'utils/ErrorHanlder';
+import { errorHandler } from 'utils/ErrorHanlder.ts';
+import  userRouter  from './route/user.route.ts';
+import cookieParser from 'cookie-parser'
 
 const app = express();
 dotenv.config
 const PORT  = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     res.json({message: "Up and running"})
 })
+
+app.use('/api/v1/user', userRouter);
 
 app.use(errorHandler);
 
