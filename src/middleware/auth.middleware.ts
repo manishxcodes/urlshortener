c348aefd136d7ace0f1a9911b1d654f216f9bd5f
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-12-05 00:54:42 +0530

    feat: add authmiddleware to verify jwt token

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,38 @@
+import type { NextFunction, Request, Response } from "express";
+import { UserRepository } from "repositories/user.repository.ts";
+import { AppError } from "utils/AppError";
+import { asyncHandler } from "utils/AsyncHandler";
+import jwt from 'jsonwebtoken';
+import type { SignupInputType } from "schema";
+
+declare global {
+    namespace Express {
+        interface Request {
+            userId?: string
+        }
+    }
+}
+
+const userRepository = new UserRepository();
+
+export const authMiddleware = asyncHandler(
+    async(req: Request, res: Response, next: NextFunction) => {
+        const authToken = req.header("Authorization") || req.cookies?.token;
+        if(!authToken) throw new AppError("Authorization missing", 401);
+
+        const jwt_secret = process.env.JWT_SECRET;
+        if(!jwt_secret) throw new AppError("Server config error. jwt secret missing", 500);
+
+        const token = authToken.split(" ")[1];
+        if(!token) throw new AppError("Invalid token", 401);
+
+        const decoded = jwt.verify(token, jwt_secret) as { id: string};
+        if(!decoded || !decoded.id) throw new AppError("Invalid token", 401);
+
+        const user = await userRepository.findById(decoded.id);
+        if(!user) throw new AppError("User no longer exists", 401);
+
+        req.userId = user.id;
+        next();
+    }
+)
