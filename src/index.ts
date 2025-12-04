af1b5cd0743f30f4b52cd838a801a21ab3d35b2d
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-12-05 03:58:58 +0530

    add urlrouter to index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { errorHandler } from 'utils/ErrorHanlder.ts';
 import  userRouter  from './route/user.route.ts';
-import cookieParser from 'cookie-parser'
+import cookieParser from 'cookie-parser';
+import urlRouter from './route/url.route.ts'
 
 const app = express();
 dotenv.config
@@ -11,11 +12,8 @@ const PORT  = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-    res.json({message: "Up and running"})
-})
-
 app.use('/api/v1/user', userRouter);
+app.use(urlRouter)
 
 app.use(errorHandler);
 
