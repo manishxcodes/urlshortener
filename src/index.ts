089bde42a2bc0521e8d7aa0512eb229950815012
Man1sh-Prasad <prasadmanish467@gmail.com>
2025-12-04 19:02:47 +0530

    add index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config
+const PORT  = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.listen(3000, () => {
+app.get("/", (req, res) => {
+    res.json({message: "Up and running"})
+})
+
+app.listen(PORT, () => {
     console.log("listening on port 3000");
 })
