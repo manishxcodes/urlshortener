cd67e97f12e2cfbb2b9877704b5f729c7c1be124
Man1sh-Prasad <prasadmanish467@gmail.com>
2026-01-18 00:28:46 +0530

    fix: properly initialize router

--- src/route/user.route.ts
+++ src/route/user.route.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+import { Router } from 'express';
 import { signin, signout, signup } from '../controllers/user.controller.ts'
 import { validate } from 'middleware/validate.middleware.ts';
 import { signinSchema, signupSchema } from 'schema.ts';
 import { authMiddleware } from 'middleware/auth.middleware.ts';
 
-const router = express();
+const router = Router();
 
 router.post('/signup', validate(signupSchema), signup);
 router.post('/signin', validate(signinSchema), signin);
