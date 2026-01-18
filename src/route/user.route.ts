d08d4701122ab79f858ca9623710c12997bc3aaa
Man1sh-Prasad <prasadmanish467@gmail.com>
2026-01-19 00:16:40 +0530

    add /me route to get auth status

--- src/route/user.route.ts
+++ src/route/user.route.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { signin, signout, signup } from '../controllers/user.controller.ts'
+import { getAuthStatus, signin, signout, signup } from '../controllers/user.controller.ts'
 import { validate } from 'middleware/validate.middleware.ts';
 import { signinSchema, signupSchema } from 'schema.ts';
 import { authMiddleware } from 'middleware/auth.middleware.ts';
@@ -8,6 +8,7 @@ const router = Router();
 
 router.post('/signup', validate(signupSchema), signup);
 router.post('/signin', validate(signinSchema), signin);
+router.get('/me', getAuthStatus)
 router.post('/signout', authMiddleware, signout);
 
 export default router;
