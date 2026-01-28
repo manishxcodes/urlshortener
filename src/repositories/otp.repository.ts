2aa718c58d8c72f75dfd14971d062aee35ca29f2
Man1sh-Prasad <prasadmanish467@gmail.com>
2026-01-28 23:56:20 +0530

    feat:  add repository layer code for otp

---
+++ src/repositories/otp.repository.ts
@@ -0,0 +1,51 @@
+import { otpsTable } from 'models/otp.model.ts';
+import { db } from '../db/index.ts';
+import { otpEntitySchema, type OtpEntityType, type OtpInsertType } from 'schema.ts';
+import { desc, eq } from 'drizzle-orm';
+
+export class OtpRepository {
+    async create (data: OtpInsertType): Promise<OtpEntityType> {
+        const [row] = await db
+            .insert(otpsTable)
+            .values(data)
+            .returning();
+
+        if (!row) throw new Error("Failed to create OTP");
+
+        return otpEntitySchema.parse({
+            ...row,
+            attempts: row.attempts ?? 0,
+            createdAt: row.createdAt ?? new Date()
+        })
+    }
+
+    async findLatestByEmail (email: string) {
+        const [row] = await db  
+            .select()
+            .from(otpsTable)
+            .where(
+                eq(otpsTable.email, email)
+            )
+            .orderBy(
+                desc(otpsTable.createdAt)
+            )
+        
+        return row || null;
+    }
+
+    async markVerified (id: string) {
+        await db.update(otpsTable)
+            .set({ verifiedAt: new Date() })
+            .where(
+                eq(otpsTable.id, id)
+            )
+    }
+
+    async incrementAttempts (id: string, attempts: number) {
+        await db.update(otpsTable)
+            .set({ attempts })
+            .where(
+                eq(otpsTable.id, id)
+            )
+    }
+}
