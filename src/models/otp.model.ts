ca2494d0d9a91c568d68c944aca5f366efefaad2
Man1sh-Prasad <prasadmanish467@gmail.com>
2026-01-30 22:29:33 +0530

    fix: spelling of expires_at

--- src/models/otp.model.ts
+++ src/models/otp.model.ts
@@ -4,7 +4,7 @@ export const otpsTable = pgTable("otp", {
     id: uuid("id").primaryKey().defaultRandom(),
     email: varchar("email", { length: 255 }).notNull(),
     otpHash: varchar("otp_hash", { length: 255 }).notNull(),
-    expiresAt: timestamp("exprires_at").notNull(),
+    expiresAt: timestamp("expires_at").notNull(),
     attempts: integer("attempts").default(0).notNull(),
     verifiedAt: timestamp("verified_at"),
     createdAt: timestamp("created_at").defaultNow()
